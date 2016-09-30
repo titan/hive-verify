2a058109ba6d28e6a4966e5e1f8856166db5f1c9
Titan <howay.tan@gmail.com>
2016-09-30 15:05:06 +0800

    add typeverification include string boolean obj num arr

--- verify.d.ts
+++ verify.d.ts
@@ -1,1 +1,6 @@
-export declare function verify(args: any[], cb: (errs: string[]) => string): boolean 
+export declare function verify(args: any[], cb: (errs: string[]) => string): boolean;
+export declare function objectVerifier(name: string, value: Object): (() => boolean | string);
+export declare function stringVerifier(name: string, value: string): (() => boolean | string);
+export declare function numberVerifier(name: string, value: number): (() => boolean | string);
+export declare function booleanVerifier(name: string, value: boolean): (() => boolean | string);
+export declare function arrayVerifier(name: string, value: any[]): (() => Boolean | string);
