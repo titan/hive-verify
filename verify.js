71af96300b64ddf5724e24a4653d3acb52f9416e
Titan <howay.tan@gmail.com>
2017-03-11 16:08:36 +0800

    remove cb and change the error message

--- verify.js
+++ verify.js
@@ -1,12 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments)).next());
-    });
-};
 function isNull(value) {
     if (value === null) {
         return false;
@@ -15,23 +7,21 @@ function isNull(value) {
         return true;
     }
 }
-function verify(args) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const errors = [];
-        for (const v of args) {
-            const result = v();
-            if (typeof result === "string") {
-                errors.push(result);
-            }
+async function verify(args) {
+    const errors = [];
+    for (const v of args) {
+        const result = v();
+        if (typeof result === "string") {
+            errors.push(result);
         }
-        if (errors.length > 0) {
-            const error = new Error("参数无法通过验证: " + errors.join(", "));
-            throw error;
-        }
-        else {
-            return true;
-        }
-    });
+    }
+    if (errors.length > 0) {
+        const error = new Error("参数无法通过验证: " + errors.join(", "));
+        throw error;
+    }
+    else {
+        return true;
+    }
 }
 exports.verify = verify;
 function objectVerifier(name, value) {
